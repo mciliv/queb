57fbaaeb89df84d512b645ebacd5523b9438baab
M <6300735+mciliv@users.noreply.github.com>
2025-09-06 19:50:24 +0300

    Fix JSON import syntax for Node.js compatibility

--- frontend/core/constants.js
+++ frontend/core/constants.js
@@ -1,7 +1,7 @@
 // Client-side constants for browser compatibility
 // Loads configuration from external config file
 
-import configData from '../config/molecular-config.json';
+import configData from '../config/molecular-config.json' with { type: 'json' };
 
 export const TEST_MOLECULES = configData.TEST_MOLECULES;
 export const SMILES_NAME_MAP = configData.SMILES_NAME_MAP;
